e08c3a4f3c6b3b05ebd096fe5155a84b2ea7c3e5
PRATYUSH PRATEEK <pprateek70@gmail.com>
2023-05-13 15:14:17 +0530

    first commit

---
+++ src/main/java/com/employee/employee/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.employee.employee.service;
+
+import java.util.List;
+
+import com.employee.employee.model.Employee;
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployeeById(long id);
+
+    void deleteEmployeeById(long id);
+}
